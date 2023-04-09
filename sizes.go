568602a5e562e9654f2025fbeec17acfb55b1111
Andreas Wachs <andreas@wachs.dk>
2023-04-09 10:25:56 +0200

    Refactor for simpler API

--- sizes.go
+++ sizes.go
@@ -18,10 +18,8 @@ func (s Size) Uint64() uint64 {
 	return uint64(s)
 }
 
-// SizeBuilder is a type that can be used to build a Size.
-// It uses the builder pattern to allow for a fluent interface.
-// The default size representation is bytes.
-type SizeBuilder struct {
+// sizeBuilder is a type that can be used to build a Size.
+type sizeBuilder struct {
 	size       Size
 	multiplier uint64
 }
@@ -31,105 +29,140 @@ type SizeBuilder struct {
 //
 
 // Bytes returns the size in bytes.
-func Bytes() *SizeBuilder {
-	return Builder().Bytes()
+func Bytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Byte).
+		setMultiplier(amount).
+		calculate()
+}
+
+// BytesAs returns the size in bytes as the specified unit.
+func BytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Byte).
+		setMultiplier(amount).
+		calculateAs(unit)
 }
 
 // Kilobytes returns the size in kilobytes.
-func Kilobytes() *SizeBuilder {
-	return Builder().Kilobytes()
+func Kilobytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Kilobyte).
+		setMultiplier(amount).
+		calculate()
+}
+
+// KilobytesAs returns the size in kilobytes as the specified unit.
+func KilobytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Kilobyte).
+		setMultiplier(amount).
+		calculateAs(unit)
 }
 
 // Megabytes returns the size in megabytes.
-func Megabytes() *SizeBuilder {
-	return Builder().Megabytes()
+func Megabytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Megabyte).
+		setMultiplier(amount).
+		calculate()
+}
+
+// MegabytesAs returns the size in megabytes as the specified unit.
+func MegabytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Megabyte).
+		setMultiplier(amount).
+		calculateAs(unit)
 }
 
 // Gigabytes returns the size in gigabytes.
-func Gigabytes() *SizeBuilder {
-	return Builder().Gigabytes()
+func Gigabytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Gigabyte).
+		setMultiplier(amount).
+		calculate()
+}
+
+// GigabytesAs returns the size in gigabytes as the specified unit.
+func GigabytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Gigabyte).
+		setMultiplier(amount).
+		calculateAs(unit)
 }
 
 // Terabytes returns the size in terabytes.
-func Terabytes() *SizeBuilder {
-	return Builder().Terabytes()
+func Terabytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Terabyte).
+		setMultiplier(amount).
+		calculate()
+}
+
+// TerabytesAs returns the size in terabytes as the specified unit.
+func TerabytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Terabyte).
+		setMultiplier(amount).
+		calculateAs(unit)
 }
 
 // Petabytes returns the size in petabytes.
-func Petabytes() *SizeBuilder {
-	return Builder().Petabytes()
+func Petabytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Petabyte).
+		setMultiplier(amount).
+		calculate()
+}
+
+// PetabytesAs returns the size in petabytes as the specified unit.
+func PetabytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Petabyte).
+		setMultiplier(amount).
+		calculateAs(unit)
 }
 
 // Exabytes returns the size in exabytes.
-func Exabytes() *SizeBuilder {
-	return Builder().Exabytes()
+func Exabytes(amount uint64) uint64 {
+	return newBuilder().
+		setSize(Exabyte).
+		setMultiplier(amount).
+		calculate()
 }
 
-// Builder returns a new SizeBuilder.
+// ExabytesAs returns the size in exabytes as the specified unit.
+func ExabytesAs(amount uint64, unit Size) uint64 {
+	return newBuilder().
+		setSize(Exabyte).
+		setMultiplier(amount).
+		calculateAs(unit)
+}
+
+// newBuilder returns a new SizeBuilder.
 // The default multiplier is 1.
-func Builder() *SizeBuilder {
-	return &SizeBuilder{
-		size: Byte,
-		// Multiplier is set to 1 by default.
-		multiplier: 1,
-	}
+func newBuilder() *sizeBuilder {
+	return &sizeBuilder{}
 }
 
-// Bytes sets the size to be in bytes.
-func (s *SizeBuilder) Bytes() *SizeBuilder {
-	s.size = Byte
+func (s *sizeBuilder) setSize(size Size) *sizeBuilder {
+	s.size = size
 	return s
 }
 
-// Kilobytes sets the size to be in kilobytes.
-func (s *SizeBuilder) Kilobytes() *SizeBuilder {
-	s.size = Kilobyte
-	return s
-}
-
-// Megabytes sets the size to be in megabytes.
-func (s *SizeBuilder) Megabytes() *SizeBuilder {
-	s.size = Megabyte
-	return s
-}
-
-// Gigabytes sets the size to be in gigabytes.
-func (s *SizeBuilder) Gigabytes() *SizeBuilder {
-	s.size = Gigabyte
-	return s
-}
-
-// Terabytes sets the size to be in terabytes.
-func (s *SizeBuilder) Terabytes() *SizeBuilder {
-	s.size = Terabyte
-	return s
-}
-
-// Petabytes sets the size to be in petabytes.
-func (s *SizeBuilder) Petabytes() *SizeBuilder {
-	s.size = Petabyte
-	return s
-}
-
-// Exabytes sets the size to be in exabytes.
-func (s *SizeBuilder) Exabytes() *SizeBuilder {
-	s.size = Exabyte
-	return s
-}
-
-// Multiply sets the multiplier for the size.
-func (s *SizeBuilder) Multiply(multiplier uint64) *SizeBuilder {
+// setMultiplier sets the multiplier for the size.
+func (s *sizeBuilder) setMultiplier(multiplier uint64) *sizeBuilder {
 	s.multiplier = multiplier
 	return s
 }
 
-// Calculate returns the calculated Size as an uint64.
-func (s *SizeBuilder) Calculate() uint64 {
+// calculate returns the calculated Size as an uint64.
+func (s *sizeBuilder) calculate() uint64 {
 	return s.size.Uint64() * s.multiplier
 }
 
-// CalculateAs returns the calculated Size in the specified unit.
-// Mind that the result is rounded down, as it is an uint64 and not a float64.
-func (s *SizeBuilder) CalculateAs(unit Size) uint64 {
-	return s.Calculate() / unit.Uint64()
+// calculateAs returns the calculated Size in the specified unit. Mind that the result is rounded down, as it is an uint64 and not a float64.
+func (s *sizeBuilder) calculateAs(unit Size) uint64 {
+	return s.calculate() / unit.Uint64()
 }
--- sizes_test.go
+++ sizes_test.go
@@ -2,258 +2,777 @@ package sizes
 
 import "testing"
 
-func TestSizeBuilderCalculate(t *testing.T) {
-
+func TestBytes(t *testing.T) {
 	tests := []struct {
-		name    string
-		builder *SizeBuilder
-		want    uint64
+		name   string
+		amount uint64
+		want   uint64
 	}{
-		// Lets test for the default multiplier and seeing that the default returned sizes are in bytes
 		{
-			name:    "Bytes",
-			builder: Builder().Bytes(),
-			want:    1,
+			name:   "1 byte",
+			amount: 1,
+			want:   1,
 		},
 		{
-			name:    "Kilobytes",
-			builder: Builder().Kilobytes(),
-			want:    1024,
+			name:   "2 bytes",
+			amount: 2,
+			want:   2,
 		},
 		{
-			name:    "Megabytes",
-			builder: Builder().Megabytes(),
-			want:    1048576,
-		},
-		{
-			name:    "Gigabytes",
-			builder: Builder().Gigabytes(),
-			want:    1073741824,
-		},
-		{
-			name:    "Terabytes",
-			builder: Builder().Terabytes(),
-			want:    1099511627776,
-		},
-		{
-			name:    "Petabytes",
-			builder: Builder().Petabytes(),
-			want:    1125899906842624,
-		},
-		{
-			name:    "Exabytes",
-			builder: Builder().Exabytes(),
-			want:    1152921504606846976,
-		},
-
-		// Now testing to see that the multiplier works
-		{
-			name:    "Bytes * 2",
-			builder: Builder().Bytes().Multiply(2),
-			want:    2,
-		},
-		{
-			name:    "Kilobytes * 2",
-			builder: Builder().Kilobytes().Multiply(2),
-			want:    2048,
-		},
-		{
-			name:    "Megabytes * 2",
-			builder: Builder().Megabytes().Multiply(2),
-			want:    2097152,
-		},
-		{
-			name:    "Gigabytes * 2",
-			builder: Builder().Gigabytes().Multiply(2),
-			want:    2147483648,
-		},
-		{
-			name:    "Terabytes * 2",
-			builder: Builder().Terabytes().Multiply(2),
-			want:    2199023255552,
-		},
-		{
-			name:    "Petabytes * 2",
-			builder: Builder().Petabytes().Multiply(2),
-			want:    2251799813685248,
-		},
-		{
-			name:    "Exabytes * 2",
-			builder: Builder().Exabytes().Multiply(2),
-			want:    2305843009213693952,
+			name:   "0 bytes",
+			amount: 0,
+			want:   0,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.builder.Calculate(); got != tt.want {
-				t.Errorf("SizeBuilder.Calculate() = %v, want %v", got, tt.want)
+			if got := Bytes(tt.amount); got != tt.want {
+				t.Errorf("Bytes() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestSizeBuilderCalculateAs(t *testing.T) {
+func TestBytesAs(t *testing.T) {
 	tests := []struct {
-		name    string
-		builder *SizeBuilder
-		unit    Size
-		want    uint64
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
 	}{
 		{
-			name:    "Bytes",
-			builder: Builder().Bytes(),
-			unit:    Byte,
-			want:    1,
+			name:   "1 byte",
+			amount: 1,
+			unit:   Byte,
+			want:   1,
 		},
 		{
-			name:    "Kilobytes",
-			builder: Builder().Kilobytes(),
-			unit:    Kilobyte,
-			want:    1,
+			name:   "0 bytes",
+			amount: 0,
+			unit:   Byte,
+			want:   0,
 		},
 		{
-			name:    "Megabytes",
-			builder: Builder().Megabytes(),
-			unit:    Megabyte,
-			want:    1,
+			name:   "1024 bytes as kilobytes",
+			amount: 1024,
+			unit:   Kilobyte,
+			want:   1,
 		},
 		{
-			name:    "Gigabytes",
-			builder: Builder().Gigabytes(),
-			unit:    Gigabyte,
-			want:    1,
+			name:   "bytes to a Megabyte",
+			amount: 1_048_576,
+			unit:   Megabyte,
+			want:   1,
 		},
 		{
-			name:    "Terabytes",
-			builder: Builder().Terabytes(),
-			unit:    Terabyte,
-			want:    1,
+			name:   "Bytes to a Gigabyte",
+			amount: 1_073_741_824,
+			unit:   Gigabyte,
+			want:   1,
 		},
 		{
-			name:    "Petabytes",
-			builder: Builder().Petabytes(),
-			unit:    Petabyte,
-			want:    1,
+			name:   "Bytes to a Terabyte",
+			amount: 1_099_511_627_776,
+			unit:   Terabyte,
+			want:   1,
 		},
 		{
-			name:    "Exabytes",
-			builder: Builder().Exabytes(),
-			unit:    Exabyte,
-			want:    1,
+			name:   "Bytes to a Petabyte",
+			amount: 1_125_899_906_842_624,
+			unit:   Petabyte,
+			want:   1,
 		},
 		{
-			name:    "Bytes * 2",
-			builder: Builder().Bytes().Multiply(2),
-			unit:    Byte,
-			want:    2,
-		},
-		{
-			name:    "Kilobytes * 2",
-			builder: Builder().Kilobytes().Multiply(2),
-			unit:    Kilobyte,
-			want:    2,
-		},
-		{
-			name:    "Megabytes * 2",
-			builder: Builder().Megabytes().Multiply(2),
-			unit:    Megabyte,
-			want:    2,
-		},
-		{
-			name:    "Gigabytes * 2",
-			builder: Builder().Gigabytes().Multiply(2),
-			unit:    Gigabyte,
-			want:    2,
-		},
-		{
-			name:    "Terabytes * 2",
-			builder: Builder().Terabytes().Multiply(2),
-			unit:    Terabyte,
-			want:    2,
-		},
-		{
-			name:    "Petabytes * 2",
-			builder: Builder().Petabytes().Multiply(2),
-			unit:    Petabyte,
-			want:    2,
-		},
-		{
-			name:    "Exabytes * 2",
-			builder: Builder().Exabytes().Multiply(2),
-			unit:    Exabyte,
-			want:    2,
-		},
-		{
-			name:    "1024 bytes to kilobytes",
-			builder: Builder().Bytes().Multiply(1024),
-			unit:    Kilobyte,
-			want:    1,
-		},
-		{
-			name:    "1024 bytes to megabytes",
-			builder: Builder().Bytes().Multiply(1024),
-			unit:    Megabyte,
-			want:    0,
+			name:   "Bytes to a Exabyte",
+			amount: 1_152_921_504_606_846_976,
+			unit:   Exabyte,
+			want:   1,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.builder.CalculateAs(tt.unit); got != tt.want {
-				t.Errorf("SizeBuilder.CalculateAs() = %v, want %v", got, tt.want)
+			if got := BytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("BytesAs() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestShorthandFunctions(t *testing.T) {
+func TestKilobytes(t *testing.T) {
 	tests := []struct {
-		name             string
-		shorthandBuilder func() *SizeBuilder
-		want             uint64
+		name   string
+		amount uint64
+		want   uint64
 	}{
 		{
-			name:             "Bytes",
-			shorthandBuilder: Bytes,
-			want:             1,
+			name:   "1 kilobyte",
+			amount: 1,
+			want:   1024,
 		},
 		{
-			name:             "Kilobytes",
-			shorthandBuilder: Kilobytes,
-			want:             1024,
+			name:   "2 kilobytes",
+			amount: 2,
+			want:   2048,
 		},
 		{
-			name:             "Megabytes",
-			shorthandBuilder: Megabytes,
-			want:             1048576,
-		},
-		{
-			name:             "Gigabytes",
-			shorthandBuilder: Gigabytes,
-			want:             1073741824,
-		},
-		{
-			name:             "Terabytes",
-			shorthandBuilder: Terabytes,
-			want:             1099511627776,
-		},
-		{
-			name:             "Petabytes",
-			shorthandBuilder: Petabytes,
-			want:             1125899906842624,
-		},
-		{
-			name:             "Exabytes",
-			shorthandBuilder: Exabytes,
-			want:             1152921504606846976,
+			name:   "0 kilobytes",
+			amount: 0,
+			want:   0,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.shorthandBuilder().Calculate(); got != tt.want {
-				t.Errorf("SizeBuilder.Calculate() = %v, want %v", got, tt.want)
+			if got := Kilobytes(tt.amount); got != tt.want {
+				t.Errorf("Kilobytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKilobytesAs(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
+	}{
+		{
+			name:   "1 kilobyte",
+			amount: 1,
+			unit:   Kilobyte,
+			want:   1,
+		},
+		{
+			name:   "0 kilobytes",
+			amount: 0,
+			unit:   Kilobyte,
+			want:   0,
+		},
+		{
+			name:   "1 kilobyte as bytes",
+			amount: 1,
+			unit:   Byte,
+			want:   1024,
+		},
+		{
+			name:   "1 kilobyte as megabytes",
+			amount: 1,
+			unit:   Megabyte,
+			want:   0,
+		},
+		{
+			name:   "1 kilobyte as gigabytes",
+			amount: 1,
+			unit:   Gigabyte,
+			want:   0,
+		},
+		{
+			name:   "1 kilobyte as terabytes",
+			amount: 1,
+			unit:   Terabyte,
+			want:   0,
+		},
+		{
+			name:   "1 kilobyte as petabytes",
+			amount: 1,
+			unit:   Petabyte,
+			want:   0,
+		},
+		{
+			name:   "1 kilobyte as exabytes",
+			amount: 1,
+			unit:   Exabyte,
+			want:   0,
+		},
+		{
+			name:   "kilotbytes to a Megabyte",
+			amount: 1_024,
+			unit:   Megabyte,
+			want:   1,
+		},
+		{
+			name:   "kilotbytes to a Gigabyte",
+			amount: 1_048_576,
+			unit:   Gigabyte,
+			want:   1,
+		},
+		{
+			name:   "kilotbytes to a Terabyte",
+			amount: 1_073_741_824,
+			unit:   Terabyte,
+			want:   1,
+		},
+		{
+			name:   "kilotbytes to a Petabyte",
+			amount: 1_099_511_627_776,
+			unit:   Petabyte,
+			want:   1,
+		},
+		{
+			name:   "kilotbytes to a Exabyte",
+			amount: 1_125_899_906_842_624,
+			unit:   Exabyte,
+			want:   1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := KilobytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("KilobytesAs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMegabytes(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		want   uint64
+	}{
+		{
+			name:   "1 megabyte",
+			amount: 1,
+			want:   1_048_576,
+		},
+		{
+			name:   "2 megabytes",
+			amount: 2,
+			want:   2_097_152,
+		},
+		{
+			name:   "0 megabytes",
+			amount: 0,
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Megabytes(tt.amount); got != tt.want {
+				t.Errorf("Megabytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMegabytesAs(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
+	}{
+		{
+			name:   "1 megabyte",
+			amount: 1,
+			unit:   Megabyte,
+			want:   1,
+		},
+		{
+			name:   "0 megabytes",
+			amount: 0,
+			unit:   Megabyte,
+			want:   0,
+		},
+		{
+			name:   "1 megabyte as bytes",
+			amount: 1,
+			unit:   Byte,
+			want:   1_048_576,
+		},
+		{
+			name:   "1 megabyte as kilobytes",
+			amount: 1,
+			unit:   Kilobyte,
+			want:   1_024,
+		},
+		{
+			name:   "1 megabyte as gigabytes",
+			amount: 1,
+			unit:   Gigabyte,
+			want:   0,
+		},
+		{
+			name:   "1 megabyte as terabytes",
+			amount: 1,
+			unit:   Terabyte,
+			want:   0,
+		},
+		{
+			name:   "1 megabyte as petabytes",
+			amount: 1,
+			unit:   Petabyte,
+			want:   0,
+		},
+		{
+			name:   "1 megabyte as exabytes",
+			amount: 1,
+			unit:   Exabyte,
+			want:   0,
+		},
+		{
+			name:   "megabytes to a Gigabyte",
+			amount: 1_048,
+			unit:   Gigabyte,
+			want:   1,
+		},
+		{
+			name:   "megabytes to a Terabyte",
+			amount: 1_048_576,
+			unit:   Terabyte,
+			want:   1,
+		},
+		{
+			name:   "megabytes to a Petabyte",
+			amount: 1_073_741_824,
+			unit:   Petabyte,
+			want:   1,
+		},
+		{
+			name:   "megabytes to a Exabyte",
+			amount: 1_099_511_627_776,
+			unit:   Exabyte,
+			want:   1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MegabytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("MegabytesAs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGigabytes(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		want   uint64
+	}{
+		{
+			name:   "1 gigabyte",
+			amount: 1,
+			want:   1_073_741_824,
+		},
+		{
+			name:   "2 gigabytes",
+			amount: 2,
+			want:   2_147_483_648,
+		},
+		{
+			name:   "0 gigabytes",
+			amount: 0,
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Gigabytes(tt.amount); got != tt.want {
+				t.Errorf("Gigabytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGigabytesAs(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
+	}{
+		{
+			name:   "1 gigabyte",
+			amount: 1,
+			unit:   Gigabyte,
+			want:   1,
+		},
+		{
+			name:   "0 gigabytes",
+			amount: 0,
+			unit:   Gigabyte,
+			want:   0,
+		},
+		{
+			name:   "1 gigabyte as bytes",
+			amount: 1,
+			unit:   Byte,
+			want:   1_073_741_824,
+		},
+		{
+			name:   "1 gigabyte as kilobytes",
+			amount: 1,
+			unit:   Kilobyte,
+			want:   1_048_576,
+		},
+		{
+			name:   "1 gigabyte as megabytes",
+			amount: 1,
+			unit:   Megabyte,
+			want:   1_024,
+		},
+		{
+			name:   "1 gigabyte as terabytes",
+			amount: 1,
+			unit:   Terabyte,
+			want:   0,
+		},
+		{
+			name:   "1 gigabyte as petabytes",
+			amount: 1,
+			unit:   Petabyte,
+			want:   0,
+		},
+		{
+			name:   "1 gigabyte as exabytes",
+			amount: 1,
+			unit:   Exabyte,
+			want:   0,
+		},
+		{
+			name:   "gigabytes to a Terabyte",
+			amount: 1_024,
+			unit:   Terabyte,
+			want:   1,
+		},
+		{
+			name:   "gigabytes to a Petabyte",
+			amount: 1_048_576,
+			unit:   Petabyte,
+			want:   1,
+		},
+		{
+			name:   "gigabytes to a Exabyte",
+			amount: 1_073_741_824,
+			unit:   Exabyte,
+			want:   1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GigabytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("GigabytesAs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTerabytes(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		want   uint64
+	}{
+		{
+			name:   "1 terabyte",
+			amount: 1,
+			want:   1_099_511_627_776,
+		},
+		{
+			name:   "2 terabytes",
+			amount: 2,
+			want:   2_199_023_255_552,
+		},
+		{
+			name:   "0 terabytes",
+			amount: 0,
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Terabytes(tt.amount); got != tt.want {
+				t.Errorf("Terabytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTerabytesAs(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
+	}{
+		{
+			name:   "1 terabyte",
+			amount: 1,
+			unit:   Terabyte,
+			want:   1,
+		},
+		{
+			name:   "0 terabytes",
+			amount: 0,
+			unit:   Terabyte,
+			want:   0,
+		},
+		{
+			name:   "1 terabyte as bytes",
+			amount: 1,
+			unit:   Byte,
+			want:   1_099_511_627_776,
+		},
+		{
+			name:   "1 terabyte as kilobytes",
+			amount: 1,
+			unit:   Kilobyte,
+			want:   1_073_741_824,
+		},
+		{
+			name:   "1 terabyte as megabytes",
+			amount: 1,
+			unit:   Megabyte,
+			want:   1_048_576,
+		},
+		{
+			name:   "1 terabyte as gigabytes",
+			amount: 1,
+			unit:   Gigabyte,
+			want:   1_024,
+		},
+		{
+			name:   "1 terabyte as petabytes",
+			amount: 1,
+			unit:   Petabyte,
+			want:   0,
+		},
+		{
+			name:   "1 terabyte as exabytes",
+			amount: 1,
+			unit:   Exabyte,
+			want:   0,
+		},
+		{
+			name:   "terabytes to a Petabyte",
+			amount: 1_024,
+			unit:   Petabyte,
+			want:   1,
+		},
+		{
+			name:   "terabytes to a Exabyte",
+			amount: 1_048_576,
+			unit:   Exabyte,
+			want:   1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TerabytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("TerabytesAs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPetabytes(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		want   uint64
+	}{
+		{
+			name:   "1 petabyte",
+			amount: 1,
+			want:   1_125_899_906_842_624,
+		},
+		{
+			name:   "2 petabytes",
+			amount: 2,
+			want:   2_251_799_813_685_248,
+		},
+		{
+			name:   "0 petabytes",
+			amount: 0,
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Petabytes(tt.amount); got != tt.want {
+				t.Errorf("Petabytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPetabytesAs(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
+	}{
+		{
+			name:   "1 petabyte",
+			amount: 1,
+			unit:   Petabyte,
+			want:   1,
+		},
+		{
+			name:   "0 petabytes",
+			amount: 0,
+			unit:   Petabyte,
+			want:   0,
+		},
+		{
+			name:   "1 petabyte as bytes",
+			amount: 1,
+			unit:   Byte,
+			want:   1_125_899_906_842_624,
+		},
+		{
+			name:   "1 petabyte as kilobytes",
+			amount: 1,
+			unit:   Kilobyte,
+			want:   1_099_511_627_776,
+		},
+		{
+			name:   "1 petabyte as megabytes",
+			amount: 1,
+			unit:   Megabyte,
+			want:   1_073_741_824,
+		},
+		{
+			name:   "1 petabyte as gigabytes",
+			amount: 1,
+			unit:   Gigabyte,
+			want:   1_048_576,
+		},
+		{
+			name:   "1 petabyte as terabytes",
+			amount: 1,
+			unit:   Terabyte,
+			want:   1_024,
+		},
+		{
+			name:   "1 petabyte as exabytes",
+			amount: 1,
+			unit:   Exabyte,
+			want:   0,
+		},
+		{
+			name:   "petabytes to a Exabyte",
+			amount: 1_024,
+			unit:   Exabyte,
+			want:   1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PetabytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("PetabytesAs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExabytes(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		want   uint64
+	}{
+		{
+			name:   "1 exabyte",
+			amount: 1,
+			want:   1_152_921_504_606_846_976,
+		},
+		{
+			name:   "2 exabytes",
+			amount: 2,
+			want:   2_305_843_009_213_693_952,
+		},
+		{
+			name:   "0 exabytes",
+			amount: 0,
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Exabytes(tt.amount); got != tt.want {
+				t.Errorf("Exabytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExabytesAs(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount uint64
+		unit   Size
+		want   uint64
+	}{
+		{
+			name:   "1 exabyte",
+			amount: 1,
+			unit:   Exabyte,
+			want:   1,
+		},
+		{
+			name:   "0 exabytes",
+			amount: 0,
+			unit:   Exabyte,
+			want:   0,
+		},
+		{
+			name:   "1 exabyte as bytes",
+			amount: 1,
+			unit:   Byte,
+			want:   1_152_921_504_606_846_976,
+		},
+		{
+			name:   "1 exabyte as kilobytes",
+			amount: 1,
+			unit:   Kilobyte,
+			want:   1_125_899_906_842_624,
+		},
+		{
+			name:   "1 exabyte as megabytes",
+			amount: 1,
+			unit:   Megabyte,
+			want:   1_099_511_627_776,
+		},
+		{
+			name:   "1 exabyte as gigabytes",
+			amount: 1,
+			unit:   Gigabyte,
+			want:   1_073_741_824,
+		},
+		{
+			name:   "1 exabyte as terabytes",
+			amount: 1,
+			unit:   Terabyte,
+			want:   1_048_576,
+		},
+		{
+			name:   "1 exabyte as petabytes",
+			amount: 1,
+			unit:   Petabyte,
+			want:   1_024,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExabytesAs(tt.amount, tt.unit); got != tt.want {
+				t.Errorf("ExabytesAs() = %v, want %v", got, tt.want)
 			}
 		})
 	}
